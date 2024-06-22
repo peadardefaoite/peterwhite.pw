e7e2fee7a812510d5ce4e450dd6a9df00064659f
Peter White <9156706+peadardefaoite@users.noreply.github.com>
2024-06-22 18:31:34 +0200

    Remove keybase proof

--- deploy.sh
+++ deploy.sh
@@ -3,23 +3,11 @@
 # ./deploy.sh 'Commit message'
 
 echo -e "Deploying updates to GitHub..."
-
-# Add content changes to git.
+# Blow away docs folder and rebuild
+rm -R docs
+hugo
+# Commit content changes to git and push.
 git add -A
-
-# Commit content changes.
 echo "Commit message: $1"
 git commit -m "$1"
-
-# Push content source
 git push origin master
-
-# Switch to gh-pages
-git checkout gh-pages
-
-# Blow away docs folder
-rm -R docs
-
-# Build the project.
-hugo
-
